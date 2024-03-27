d4ee0966af8619f00608323dc8d45d5bc6a3773c
CarinaQi <qiyue1116@gmail.com>
2024-03-27 13:13:27 +1300

    fixed like button page flash problem

--- public/script/button.js
+++ public/script/button.js
@@ -6,7 +6,7 @@ window.addEventListener("load", function () {
   async function displayLikeButton() {
     const likeBtn = document.querySelector(".like-btn");
     const likeIcon = document.getElementById("like-icon");
-
+    const likeCount = document.getElementById("like_count");
 
     const user_id = document.querySelector("#user_id")
     const article_id = document.querySelector("#article_id")
@@ -22,14 +22,18 @@ window.addEventListener("load", function () {
     }
 
     likeBtn.addEventListener("click", () => {
-      if (isLiked == 1) {
-        likeIcon.classList.replace("fa-solid", "fa-regular");
-        likeIcon.classList.remove("liked");
-        removeLike(article_id.value)
-      } else {
-        likeIcon.classList.replace("fa-regular", "fa-solid");
-        likeIcon.classList.add("liked");
-        addLike(article_id.value)
+      if(user_id.value){
+        if (isLiked == 1) {
+          likeIcon.classList.replace("fa-solid", "fa-regular");
+          likeIcon.classList.remove("liked");
+          removeLike(article_id.value, likeCount)
+          isLiked = 0;
+        } else {
+          likeIcon.classList.replace("fa-regular", "fa-solid");
+          likeIcon.classList.add("liked");
+          addLike(article_id.value, likeCount);
+          isLiked = 1;
+        }
       }
     })
 
@@ -47,11 +51,11 @@ window.addEventListener("load", function () {
     return isLiked;
   }
 
-  async function removeLike(article_id) {
+  async function removeLike(article_id, likeCount) {
     fetch(`/removeLike?id=${article_id}`)
       .then(response => {
         if (response.status === 200) {
-          location.reload();
+          likeCount.textContent--;
         } else {
           console.error('Error removing like');
         }
@@ -61,11 +65,11 @@ window.addEventListener("load", function () {
       });
   }
 
-  async function addLike(article_id) {
+  async function addLike(article_id, likeCount) {
     fetch(`/addLike?id=${article_id}`)
       .then(response => {
         if (response.status === 200) {
-          location.reload();
+          likeCount.textContent++;
         } else {
           console.error('Error adding like');
         }
