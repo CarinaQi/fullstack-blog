73a59a877d681eef73751066be14611b58f1542c
CarinaQi <qiyue1116@gmail.com>
2024-03-27 15:17:12 +1300

    fix the flash bug on sub page

--- public/script/sub.js
+++ public/script/sub.js
@@ -1,29 +1,39 @@
 window.addEventListener("load", function () {
 
     const btn_unsubscription = document.querySelectorAll(".btn_unsubscription");
+    const subscription_number = document.querySelector("#subscription_number");
+    let subscription_count = subscription_number.innerText.match(/\d+/g);
     btn_unsubscription.forEach(async (btn) => {
         const idValue = btn.querySelector(".subscription_id");
         const subscription_id = idValue.value
-        btn.addEventListener("click", function () {
-            removeSubscription(subscription_id);
+        btn.addEventListener("click", function (event) {
+            removeSubscription(subscription_id, event);
         });
     })
-
+    
+    const subscriber_number = document.querySelector("#subscriber_number");
+    let subscriber_count = subscriber_number.innerText.match(/\d+/g);
     const btn_unsubscriber = document.querySelectorAll(".btn_unsubscriber");
     btn_unsubscriber.forEach(async (btn) => {
         const idValue = btn.querySelector(".subscriber_id");
         const subscriber_id = idValue.value
-        btn.addEventListener("click", function () {
-            removeSubscriber(subscriber_id);
+        btn.addEventListener("click", function (event) {
+            removeSubscriber(subscriber_id, event);
         });
     })
 
-    async function removeSubscription(subscription_id) {
+    async function removeSubscription(subscription_id, event) {
         const subscriptionId = subscription_id;
         fetch(`/removeSubscription?id=${subscriptionId}`)
             .then(response => {
                 if (response.status === 200) {
-                    location.reload();
+                    event.target.closest(".subscription_rows").remove();
+                    subscription_count--;
+                    if(subscription_count != 0){
+                        subscription_number.innerText = `${subscription_count} Subscriptions`
+                    } else if(subscription_count == 0){
+                        subscription_number.innerText = `${subscription_count} Subscription`
+                    }
                 } else {
                     console.error('Error removing subscription');
                 }
@@ -33,14 +43,20 @@ window.addEventListener("load", function () {
             });
     }
 
-    async function removeSubscriber(subscriber_id) {
+    async function removeSubscriber(subscriber_id, event) {
         const subscriberId = subscriber_id;
         fetch(`/removeSubscriber?id=${subscriberId}`)
             .then(response => {
                 if (response.status === 200) {
-                    location.reload();
+                    event.target.closest(".subscriber_rows").remove();
+                    subscriber_count--;
+                    if(subscriber_count != 0){
+                        subscriber_number.innerText = `${subscriber_count} Subscribers`
+                    } else if(subscriber_count == 0){
+                        subscriber_number.innerText = `${subscriber_count} Subscriber`
+                    }
                 } else {
-                    console.error('Error removing subscription');
+                    console.error('Error removing subscriber');
                 }
             })
             .catch(error => {
